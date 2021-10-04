312744bf46c895540681cb5d3bbfbcf3987e7f9a
Meylin Reyes <reyesmeylin20@gmail.com>
2021-10-04 12:54:01 -0600

    Nueva rama de Meylin

--- AsignacionSistCuenta3009/PlanPenForm.cs
+++ AsignacionSistCuenta3009/PlanPenForm.cs
@@ -19,7 +19,34 @@ namespace AsignacionSistCuenta3009
 
         private void PlanPensiones_Load(object sender, EventArgs e)
         {
-            
+            PlanPenciones cuentaPencion = new PlanPenciones();
+            PlanPenciones pencion = new PlanPenciones();
+            cuentaPencion.numero = 010101010101;
+            cuentaPencion.interes = 0.15F;
+            cuentaPencion.titular = "Meylin Nohely Reyes Medina";
+            //cuentaPencion.saldo = cuentaPencion.ingreso() - pencion.cotizacion;
+
+            lbNumero.Text = cuentaPencion.numero.ToString();
+            lbNombre.Text = cuentaPencion.titular.ToString();
+            //IbSaldo.Text = cuentaPencion.saldo.ToString();
+
+            lbIngreso.Text = cuentaPencion.ingreso().ToString();
+            //IbIngreMes.Text = cuentaPencion.ingresoInteresmes().ToString();
+
+            //IbLimite.Text = cuentaPencion.transferirHasta().ToString();
+            lbPorcentCoti.Text = (cuentaPencion.interes * 100).ToString() + "%";
+            //IbResumenCtn.Text = cuentaPencion.leerSaldo();
+
+            //vencimiento de la cuenta
+            pencion.vencimiento = new DateTime(2021, 10, 30);
+            pencion.cotizacion = cuentaPencion.ingreso() * cuentaPencion.interes;
+            lbMontoCoti.Text = pencion.cotizacion.ToString();
+
+            cuentaPencion.saldo = cuentaPencion.ingreso() - pencion.cotizacion;
+            lbSaldo.Text = cuentaPencion.saldo.ToString();
+
+            pencion.vencimiento = new DateTime(2021, 10, 30);
+            lbFecha.Text = pencion.vencimiento.ToString();
         }
     }
 }
--- AsignacionSistCuenta3009/TransaccionReti.cs
+++ AsignacionSistCuenta3009/TransaccionReti.cs
@@ -19,14 +19,24 @@ namespace AsignacionSistCuenta3009
 
         private void TransaccionReti_Load(object sender, EventArgs e)
         {
-            
-            
-            
+            CuentaCorriente cuentaCorre = new CuentaCorriente();
+            cuentaCorre.numero = 01010101010101010;
+            cuentaCorre.titular = "Harold Enoc Santos Morillo";
+
+            lbNumero.Text = cuentaCorre.numero.ToString();
+            lbNombre.Text = cuentaCorre.titular;
+            lbIngreso.Text = cuentaCorre.ingreso().ToString();
         }
 
         private void btnRealizar_Click(object sender, EventArgs e)
         {
-            
+            CuentaCorriente cuentaCorre = new CuentaCorriente();
+            var monto = float.Parse(tbxMontRetirar.Text);
+            var ingreso = cuentaCorre.ingreso();
+            cuentaCorre.saldo = cuentaCorre.reIntegro(ingreso, monto);
+
+            lbSaldo.Text = cuentaCorre.saldo.ToString();
+
         }
     }
 }
